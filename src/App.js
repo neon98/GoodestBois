142b5ed9ff47d844a4117bafd4c86f3dd5e78aeb
Ekta <ektap.098@gmail.com>
2019-06-26 00:46:47 +0530

    Added top level navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Media from 'react-media';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config';
 
 import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
@@ -12,10 +14,92 @@ import TweetsPage from './components/TweetsPage';
 
 import './App.css';
 
+firebase.initializeApp(firebaseConfig);
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      userID: '',
+      profileOwnerID: '',
+      currentPage: 'Home',
+      openLoginModal: false,
+      openSignUpModal: false,
+      mobileUI: false
+    }
+
+    this.setUser = this.setUser.bind(this);
+    this.resetUser = this.resetUser.bind(this);
+    this.setProfileOwnerID = this.setProfileOwnerID.bind(this)
+    this.resetProfileOwnerID = this.resetProfileOwnerID.bind(this)
+
+    this.setPage = this.setPage.bind(this);
+
+    this.openLoginModal = this.openLoginModal.bind(this);
+    this.closeLoginModal = this.closeLoginModal.bind(this);
+    this.openSignUpModal = this.openSignUpModal.bind(this);
+    this.closeSignUpModal = this.closeSignUpModal.bind(this);
+    this.openProfilePage = this.openProfilePage.bind(this);
+  }
+  setUser(userID) {
+    this.setState({
+      userID: userID,
+      profileOwnerID: userID,
+    });
+  }
+  resetUser() {
+    localStorage.removeItem('doggositeuser');
+    this.setState({
+      userID: '',
+      profileOwnerID: ''
+    });
+  }
+  setProfileOwnerID(profileOwnerID) {
+    this.setState({
+      profileOwnerID: profileOwnerID
+    });
+  }
+  resetProfileOwnerID() {
+    this.setState({
+      profileOwnerID: this.state.userID
+    });
+  }
+  setPage(page) {
+    this.setState({
+      currentPage: page
+    })
+    if (page !== 'Profile') {
+      this.resetProfileOwnerID()
+    }
+  }
+  openLoginModal() {
+    this.setState({
+      openLoginModal: true
+    })
+  }
+  closeLoginModal() {
+    this.setState({
+      openLoginModal: false
+    })
+  }
+  openSignUpModal() {
+    this.setState({
+      openSignUpModal: true
+    })
+  }
+  closeSignUpModal() {
+    this.setState({
+      openSignUpModal: false
+    })
+  }
+  openProfilePage(profileOwnerID) {
+    this.setProfileOwnerID(profileOwnerID);
+    this.setPage("Profile");
+  }
+  componentDidMount() {
+    var userID = localStorage.getItem('doggositeuser');
+    if (userID) {
+      this.setUser(userID)
     }
   }
   render() {
@@ -34,10 +118,10 @@ class App extends React.Component {
         currentPage = <TweetsPage />
         break;
       case "Profile":
-        currentPage = <ProfilePage/>
+        currentPage = <ProfilePage />
         break;
       default:
-        currentPage = <HomePage/>
+        currentPage = <HomePage />
         break;
     }
     return (
@@ -52,14 +136,31 @@ class App extends React.Component {
           }
         />
         <div className="header">
-        <Navbar />
+          <Navbar
+            userID={this.state.userID}
+            openLoginModal={this.openLoginModal}
+            openSignUpModal={this.openSignUpModal}
+            mobileUI={this.state.mobileUI}
+            setPage={this.setPage}
+            resetUser={this.resetUser}
+          />
           {
-            false ?
-              <LoginForm /> : null
+            this.state.openLoginModal ?
+              <LoginForm
+                firebase={firebase}
+                setUser={this.setUser}
+                open={this.state.openLoginModal}
+                close={this.closeLoginModal}
+              /> : null
           }
           {
-            false ?
-              <SignUpForm /> : null
+            this.state.openSignUpModal ?
+              <SignUpForm
+                firebase={firebase}
+                setUser={this.setUser}
+                open={this.state.openSignUpModal}
+                close={this.closeSignUpModal}
+              /> : null
           }
         </div>
         <div className="content">
